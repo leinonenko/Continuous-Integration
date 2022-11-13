b04330522f092d1e524c218d8366baeadfb8c6d2
Pavlo Leinonen <leinonenko@gmail.com>
2022-11-13 19:21:02 +0200

    trying to push corrupted code

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (_req, res) =>  {
-    res.status(200).send("Hello world")
+    res.status(200).send("Hello, this is Pavlo testing CI")
 })
-module.exports = app;
+modulde.exports = app;
